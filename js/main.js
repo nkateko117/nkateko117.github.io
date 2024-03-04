3c2e056e85239300464452eb3971bb807ce9e718
Nkateko Maluleke <nkateko.maluleke03@gmail.com>
2024-03-04 17:57:41 +0200

    2 projects moved to modals

--- js/main.js
+++ js/main.js
@@ -95,21 +95,32 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Get the modal
 var modal = document.getElementById("myModal");
+var planUpModal = document.getElementById("myPlanUpModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("openModalBtn");
+var planbtn = document.getElementById("openPlanUpModalBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close2")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
+planbtn.onclick = function() {
+    planUpModal.style.display = "block";
+  }
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+    modal.style.display = "none";
+  }
+
+span2.onclick = function() {
+  planUpModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
@@ -118,5 +129,11 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+window.onclick = function(event) {
+    if (event.target == planUpModal) {
+      planUpModal.style.display = "none";
+    }
+  }
 
 //-----------------------------------Modal Ends--------------------------------------
